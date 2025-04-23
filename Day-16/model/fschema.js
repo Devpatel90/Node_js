72ae69664a9eee343ca450d68b14b47912aab94f
Devpatel90 <devsojitra02@gmail.com>
2025-04-23 16:24:53 +0530

    File Added

--- Day-16/model/fschema.js
+++ Day-16/model/fschema.js
@@ -1,6 +1,10 @@
 const mongoose = require("mongoose");
 
 const schema = mongoose.Schema({
+    profile:{
+        type : String,
+        required : true,    
+    },
     fname : {
         type : String,
         required : true
--- Day-16/routes/route.js
+++ Day-16/routes/route.js
@@ -1,16 +1,25 @@
 const express = require('express');
+const multer = require('../middleware/multer');
 
 const route = express.Router();
 const ctl = require('../controller/ctl');
 
-route.get('/', ctl.dashboard);
+route.get("/",ctl.login)
+
+route.post("/login",ctl.loginadmin);
+
+route.get('/dashboard', ctl.dashboard);
 
 route.get('/addAdmin', ctl.addAdmin);
 
 route.get('/table', ctl.table);
 
-route.post('/addrecord', ctl.add);
+route.post('/addrecord',multer, ctl.add);
 
 route.get("/deldata",ctl.delete);
+
+route.get("/editdata",ctl.edit);
+
+route.post("/updaterec",multer,ctl.update);
 
 module.exports = route;
